e00deaad88b91f7e99a8fc96c5f56539f94e8b04
Matthew Candelaria <matthewcandelaria14@gmail.com>
2018-07-12 16:22:34 +0000

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+	
+  get 'users/new'
 	root 'static_pages#home'
-	get 'static_pages/home'
-	get 'static_pages/help'
-	get 'static_pages/about'
+	get  '/help',    to: 'static_pages#help' 	#creates two named routes help_path and help_url
+	get  '/about',   to: 'static_pages#about'
+	get  '/contact', to: 'static_pages#contact'
+	get  '/signup',  to: 'users#new'
 end
